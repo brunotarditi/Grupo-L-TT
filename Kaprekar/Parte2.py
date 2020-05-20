6d9b7235b991bf4f46705da8707bccd08a4aa082
Lautaro-Panella <raull.panella@alumnos.frm.utn.edu.ar>
2020-05-19 23:57:43 -0300

    Modifico el archivo parte2.py.

---
+++ Kaprekar/Parte2.py
@@ -0,0 +1,19 @@
+def buscarConstante(lista, cantidad):
+    for k in range(cantidad):
+        numero = lista[k]
+        n = int(numero)
+        for k in range(10):
+            numero = "{:04d}".format(n)
+            if n == 6174:
+                k = -1
+                break
+            descendente = "".join(sorted(numero,reverse = True))
+            ascendente = "".join(sorted(numero))
+            n = int(descendente) - int(ascendente)
+            if n == 0:
+                k = 7
+                break
+            if n == 6174:
+                break
+        print("--------------")
+        print(k + 1)
