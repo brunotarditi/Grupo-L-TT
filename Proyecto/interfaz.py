c7f5d5b905134e550c58a86af6f425d0120738f2
Lautaro-Panella <raull.panella@alumnos.frm.utn.edu.ar>
2020-06-10 17:23:27 -0300

    Agrego metodos ejecutar consultas a db.

--- Proyecto/interfaz.py
+++ Proyecto/interfaz.py
@@ -28,6 +28,24 @@ class Vista:
         ttk.Button(text='Desplegar', command=self.desplegar).grid(row=5, column=0, sticky=W + E)
         ttk.Button(text='Agregar').grid(row=5, column=4, sticky=W + E)
 
+    def run_query(self, query, parameters=()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    def get_products(self):
+
+        # Consultando datos
+        records = self.tree.get_children()
+        for elementos in records:
+            self.tree.delete(elementos)
+        query = 'SELECT * FROM CATEGORIAS ORDER BY ID DESC'
+        db_rows = self.run_query(query)
+        for row in db_rows:
+            self.tree.insert('', 0, text=row[0], values=row[1])
+
 if __name__ == '__main__':
     ventana = Tk()
     app = Vista(ventana)
